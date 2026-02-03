0d738e917c4651a19aadf77755df43b0d7d47cc8
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-02-03 17:17:09 +0800

    index returns json

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -20,6 +20,10 @@ class KittensController < ApplicationController
 
   def index
     @kittens=Kitten.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+    end
   end
 
   def update
